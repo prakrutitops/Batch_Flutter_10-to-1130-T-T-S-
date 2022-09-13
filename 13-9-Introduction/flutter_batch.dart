b0e6eefbd89c65b391bc1bb4b9470f7aaa88335c
prakrutitops <109515514+prakrutitops@users.noreply.github.com>
2022-09-13 11:20:17 +0530

    Add files via upload

---
+++ 13-9-Introduction/flutter_batch.dart
@@ -0,0 +1,27 @@
+void main(List<String> arguments)
+{
+
+      int marks =56;
+
+      if(marks>=70)
+      {
+          print("A Grade");
+      }
+      else if(marks>=60)
+      {
+        print("B Grade");
+      }
+      else if(marks>=50)
+      {
+          print("C Grade");
+      }
+      else if(marks>=40)
+      {
+          print("D Grade");
+      }
+      else
+      {
+          print("Fail");
+      }
+
+}
